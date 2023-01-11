2d6c5b979233d555454a54ba4a75552a35ad3ca1
Fabiana <fabiana.liria@udla.edu.ec>
2023-01-11 10:05:20 -0500

    Tiene la routa

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace FLAppBurger;
+﻿using FLAppBurger.Views;
+
+namespace FLAppBurger;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+		Routing.RegisterRoute(nameof(FLBurgerItemPage), typeof(FLBurgerItemPage));
+    }
 }
