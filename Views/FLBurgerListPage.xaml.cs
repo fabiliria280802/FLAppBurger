866eec6b5ff5b45b3923e91506d81f3e4886dfc7
Fabiana <fabiana.liria@udla.edu.ec>
2023-01-16 15:57:53 -0500

    Se quitan todas las lineas innecesarias dentro de FLBurgerListPage.xaml y FLBurgerListPage.xaml.cs

--- Views/FLBurgerListPage.xaml.cs
+++ Views/FLBurgerListPage.xaml.cs
@@ -6,13 +6,19 @@ public partial class FLBurgerListPage : ContentPage
     public FLBurgerListPage()
     {
         InitializeComponent();
-        List<FLBurger> flburger = App.FLBurgerRepo.GetAllBurgers();
-        burgerList.ItemsSource = flburger;
-        MessagingCenter.Subscribe<FLBurgerItemPage>(this, "itemAdded", (sender) => OnUpdate(sender, EventArgs.Empty));
+        BindingContext= this;
+        //List<FLBurger> flburger = App.FLBurgerRepo.GetAllBurgers();
+        //burgerList.ItemsSource = flburger;
+        //MessagingCenter.Subscribe<FLBurgerItemPage>(this, "itemAdded", (sender) => OnUpdate(sender, EventArgs.Empty));
     }
-    async void OnItemAdded(object sender, EventArgs e)
+    //async void OnItemAdded(object sender, EventArgs e) ya no es necesario
+    void OnItemAdded(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync(nameof(FLBurgerItemPage));
+        //await 
+        Shell.Current.GoToAsync(nameof(FLBurgerItemPage), true, new Dictionary<string, object> 
+        { 
+            ["Item"] = new FLBurger()
+        });
     }
 
     private void OnUpdate(object sender, EventArgs e)
@@ -20,11 +26,16 @@ public partial class FLBurgerListPage : ContentPage
         //fetch new items and update the Burgers property
         List<FLBurger> newBurgers = App.FLBurgerRepo.GetAllBurgers();
         burgerList.ItemsSource = newBurgers;
-        AddBurger?.Invoke(this, EventArgs.Empty);
+        //AddBurger?.Invoke(this, EventArgs.Empty);
     }
 
     private void OnUpdateButtonClicked(object sender, EventArgs e)
     {
         OnUpdate(sender, e);
+    }
+
+    private void burgerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
     }
 }
